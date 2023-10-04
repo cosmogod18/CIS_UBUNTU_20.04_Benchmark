b59589e7cc812426f4430dfdd6e30597f6e95c02
robotiaga <32516858+cosmogod18@users.noreply.github.com>
2023-10-04 09:43:10 +0300

    Update 1.1.1.1-1.1.17_Ensure_unused_filesystems_disabled.sh

--- 1.1.1 Disable unused filesystems/1.1.1.1-1.1.17_Ensure_unused_filesystems_disabled.sh
+++ 1.1.1 Disable unused filesystems/1.1.1.1-1.1.17_Ensure_unused_filesystems_disabled.sh
@@ -1,83 +1,82 @@
 #!/usr/bin/env bash
 
 # List of modules to check
-modules=("cramfs", "freevxfs", "jffs2", "hfs", "hfsplus", "squashfs", "udf")
+module_names=("cramfs" "freevxfs" "jffs2" "hfs" "hfsplus" "squashfs" "udf")
 
-# Initialize variables
-l_output=""
-l_output2=""
-l_output3=""
-l_dl=""
 l_mtype="fs"
 l_searchloc="/lib/modprobe.d/*.conf /usr/local/lib/modprobe.d/*.conf /run/modprobe.d/*.conf /etc/modprobe.d/*.conf"
 
-module_loadable_chk() {
-    # Check if the module is currently loadable
-    l_loadable="$(modprobe -n -v "$1")"
-    if [[ $(wc -l <<< "$l_loadable") -gt 1 ]]; then
-        l_loadable="$(grep -P -- "(^\h*install|\b$1)\b" <<< "$l_loadable")"
-    fi
+module_check() {
+    local l_mname="$1"
+    local l_output=""
+    local l_output2=""
+    local l_output3=""
+    local l_dl=""
+    local l_mpath="/lib/modules/**/kernel/$l_mtype"
+    local l_mpname="$(tr '-' '_' <<< "$l_mname")"
+    local l_mndir="$(tr '-' '/' <<< "$l_mname")"
 
-    if grep -Pq -- '^\h*install \/bin\/(true|false)' <<< "$l_loadable"; then
-        l_output="$l_output\n - module: \"$1\" is not loadable: \"$l_loadable\""
-    else
-        l_output2="$l_output2\n - module: \"$1\" is loadable: \"$l_loadable\""
-    fi
-}
+    module_loadable_chk() {
+        # Check if the module is currently loadable
+        l_loadable="$(modprobe -n -v "$l_mname")"
+        if [[ $(wc -l <<< "$l_loadable") -gt 1 ]]; then
+            l_loadable="$(grep -P -- "(^h*install|\b$l_mname)\b" <<< "$l_loadable")"
+        fi
 
-module_loaded_chk() {
-    # Check if the module is currently loaded
-    if ! lsmod | grep -w "$1" > /dev/null 2>&1; then
-        l_output="$l_output\n - module: \"$1\" is not loaded"
-    else
-        l_output2="$l_output2\n - module: \"$1\" is loaded"
-    fi
-}
+        if grep -Pq -- '^\h*install \/bin\/(true|false)' <<< "$l_loadable"; then
+            l_output="$l_output\n - module: \"$l_mname\" is not loadable: \"$l_loadable\""
+        else
+            l_output2="$l_output2\n - module: \"$l_mname\" is loadable: \"$l_loadable\""
+        fi
+    }
 
-module_deny_chk() {
-    # Check if the module is deny-listed
-    l_dl="y"
-    local mpname="$(tr '-' '_' <<< "$1")"
-    if modprobe --showconfig | grep -Pq -- '^\h*blacklist\h+'"$mpname"'\b'; then
-        l_output="$l_output\n - module: \"$1\" is deny listed in: \"$(grep -Pls -- "^\h*blacklist\h+$mpname\b" $l_searchloc)\""
-    else
-        l_output2="$l_output2\n - module: \"$1\" is not deny listed"
-    fi
-}
+    module_loaded_chk() {
+        # Check if the module is currently loaded
+        if ! lsmod | grep -w "$l_mname" > /dev/null 2>&1; then
+            l_output="$l_output\n - module: \"$l_mname\" is not loaded"
+        else
+            l_output2="$l_output2\n - module: \"$l_mname\" is loaded"
+        fi
+    }
 
-# Check each module in the list
-for module in "${modules[@]}"; do
-    l_output=""
-    l_output2=""
-    l_output3=""
-    l_dl=""
-    l_mname="$module"
-    l_mpath="/lib/modules/**/kernel/$l_mtype"
-    l_mndir="$(tr '-' '/' <<< "$l_mname")"
+    module_deny_chk() {
+        # Check if the module is deny-listed
+        l_dl="y"
+        if modprobe --showconfig | grep -Pq -- '^\h*blacklist\h+'"$l_mpname"'\b'; then
+            l_output="$l_output\n - module: \"$l_mname\" is deny listed in: \"$(grep -Pls -- "^\h*blacklist\h+$l_mname\b" $l_searchloc)\""
+        else
+            l_output2="$l_output2\n - module: \"$l_mname\" is not deny listed"
+        fi
+    }
 
     # Check if the module exists on the system
     for l_mdir in $l_mpath; do
         if [ -d "$l_mdir/$l_mndir" ] && [ -n "$(ls -A "$l_mdir/$l_mndir")" ]; then
             l_output3="$l_output3\n - \"$l_mdir\""
-            [ "$l_dl" != "y" ] && module_deny_chk "$module"
+            [ "$l_dl" != "y" ] && module_deny_chk
             if [ "$l_mdir" = "/lib/modules/$(uname -r)/kernel/$l_mtype" ]; then
-                module_loadable_chk "$module"
-                module_loaded_chk "$module"
+                module_loadable_chk
+                module_loaded_chk
             fi
         else
-            l_output="$l_output\n - module: \"$module\" doesn't exist in \"$l_mdir\""
+            l_output="$l_output\n - module: \"$l_mname\" doesn't exist in \"$l_mdir\""
         fi
     done
 
-    # Report results for each module
+    # Report results. If no failures output in l_output2, we pass
     if [ -n "$l_output3" ]; then
-        echo -e "\n\n -- INFO --\n - module: \"$module\" exists in:$l_output3"
+        echo -e "\n\n -- INFO --\n - module: \"$l_mname\" exists in:$l_output3"
     fi
 
     if [ -z "$l_output2" ]; then
-        echo -e "\n- Audit Result for module \"$module\":\n ** PASS **\n$l_output\n"
+        echo -e "\n- Audit Result for module \"$l_mname\":\n ** PASS **\n$l_output\n"
     else
-        echo -e "\n- Audit Result for module \"$module\":\n ** FAIL **\n - Reason(s) for audit failure:\n$l_output2\n"
+        echo -e "\n- Audit Result for module \"$l_mname\":\n ** FAIL **\n - Reason(s) for audit failure:\n$l_output2\n"
         [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
     fi
+}
+
+# Check each module in the list
+for module_name in "${module_names[@]}"; do
+    module_check "$module_name"
 done
